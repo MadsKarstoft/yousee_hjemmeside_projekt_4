2c55d0c69492ca6355c0a53b96f50946fc3f3b0a
MadsKarstoft <mads394@gmail.com>
2019-03-06 10:05:40 +0100

    Tilføjer javascript og billeder og navigationsmenu

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+let navigationsmenu = document.getElementById('menu_javascript');
+let navKnapBillede = document.getElementById('nav_skift_javascript');
+navKnapBillede.addEventListener('click', function () {
+navigationsmenu.classList.toggle('active');
+
+}) ;
